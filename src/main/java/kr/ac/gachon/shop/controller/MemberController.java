a215fe8262ee4e45a4ca4822ee26f7f392e8afa1
gustjdw <johs710@gachon.ac.kr>
2022-11-22 22:54:28 +0900

    first commit

---
+++ src/main/java/kr/ac/gachon/shop/controller/MemberController.java
@@ -0,0 +1,22 @@
+package kr.ac.gachon.shop.controller;
+
+import kr.ac.gachon.shop.dto.MemberFormDto;
+import kr.ac.gachon.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+}
