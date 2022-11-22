8bdb823b3720b9891e3721eca9bad1829f04196a
gustjdw <johs710@gachon.ac.kr>
2022-11-22 23:43:37 +0900

    2022-1-22

---
+++ src/main/java/kr/ac/gachon/shop/controller/MainController.java
@@ -0,0 +1,17 @@
+package kr.ac.gachon.shop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index(Model model) {
+        log.info("main 컨트롤러");
+        return "main";
+    }
+}
--- src/main/java/kr/ac/gachon/shop/service/MemberService.java
+++ src/main/java/kr/ac/gachon/shop/service/MemberService.java
@@ -3,6 +3,10 @@ package kr.ac.gachon.shop.service;
 import kr.ac.gachon.shop.entity.Member;
 import kr.ac.gachon.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,16 +14,34 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor  // bean 주입 방법 생성자 final member, @NonNull member 생성자 생성함
-public class MemberService {
+public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
+
     public Member saveMember(Member member) {
         validateDuplicateMember(member);
         return memberRepository.save(member);
     }
+
     private void validateDuplicateMember(Member member) {
         Member findMember = memberRepository.findByEmail(member.getEmail());
         if (findMember != null) {
             throw new IllegalStateException("이미 가입된 회원입니다.");  // 예외 처리
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
